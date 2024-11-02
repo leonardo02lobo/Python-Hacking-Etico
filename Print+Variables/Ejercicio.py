e0540f7c3eb504648ee7895fb291de650608672b
leonardo lobo <loboleonardo75@gmail.com>
2024-11-02 09:17:10 -0400

    Variables y un ejercicio practico

---
+++ Print+Variables/Ejercicio.py
@@ -0,0 +1,5 @@
+firtname = input('what is your  firt name?: ')
+
+lastname = input('what is your last name?: ')
+
+print(f'your name is {firtname} {lastname}')
---
+++ Print+Variables/Variables Practice.py
@@ -0,0 +1,13 @@
+x = 5
+y = 7
+
+print(x+y)
+
+y = "Lobo"
+
+print(str(x) + y)
+
+x = "5"
+y = 7
+
+print(int(x)+y)
