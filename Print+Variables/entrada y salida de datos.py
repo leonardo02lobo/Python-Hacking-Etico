4a32ca71b7ed0162f86b76d973242d17e46fa369
leonardo lobo <loboleonardo75@gmail.com>
2024-11-01 23:17:28 -0400

    Primeros pasos en python

---
+++ Print+Variables/entrada y salida de datos.py
@@ -0,0 +1,12 @@
+x = 'Leonardo'
+y = 'Lobo'
+
+print('hello world')
+print('hello world1 \nhello world 2')
+print('Leonardo '+'Lobo')
+print(x+' '+y)
+print(f'{x} {y}')
+
+ip = input('what is the target ip?')
+
+print(ip)
